663c13235b5accd9ed784d43c4c3126b62fb9e0b
Ross Roberts <ross123roberts@gmail.com>
2022-01-23 14:38:46 -0500

    initial stubbing out of web forms framework

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
-
+from config_files.config import Config
 
 app = Flask(__name__)
-
+app.config.from_object(Config
+                       )
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from collections import namedtuple
+from app.forms import LoginForm
 
 Book = namedtuple('Book', 'title author genre release_date read rating complete_date tags')
 
@@ -33,3 +34,12 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f'Login requested for user {form.username.data}, remember_me={form.remember_me.data}')
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
