efaad32f560afe17fc81e1a574c2143fe70e6abb
Ross Roberts <ross123roberts@gmail.com>
2022-01-23 13:41:30 -0500

    added base.html for overall page structure

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,9 @@
 from app import app
 from flask import render_template
+from collections import namedtuple
+
+Book = namedtuple('Book', 'title author genre release_date read rating complete_date tags')
+
 @app.route('/')
 @app.route('/index')
 def index():
@@ -7,11 +11,25 @@ def index():
     posts = [
         {
             'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
+            'record': Book('The Lies of Locke Lamora',
+                           'Scott Lynch',
+                           'Fantasy',
+                           '2006-06-27',
+                           1,
+                           95,
+                           "01-01-2015",
+                           "Gentleman Bastards")
         },
         {
             'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was awesome!'
+            'record': Book('The Way of Kings',
+                           'Brandon Sanderson',
+                           'Fantasy',
+                           '2010-08-31',
+                           1,
+                           91,
+                           "01-01-2019",
+                           "Cosmere Stormlight Archive")
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
