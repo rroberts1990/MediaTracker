8c5911d863701a2c268e239574fc32474edb43db
Ross Roberts <ross123roberts@gmail.com>
2022-01-26 08:28:57 -0500

    abstarcted out user posts

--- app/routes.py
+++ app/routes.py
@@ -12,31 +12,8 @@ Book = namedtuple('Book', 'title author genre publish_date read rating complete_
 @app.route('/index')
 @login_required
 def index():
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'record': Book('The Lies of Locke Lamora',
-                           'Scott Lynch',
-                           'Fantasy',
-                           '2006-06-27',
-                           1,
-                           95,
-                           "01-01-2015",
-                           "Gentleman Bastards")
-        },
-        {
-            'author': {'username': 'Susan'},
-            'record': Book('The Way of Kings',
-                           'Brandon Sanderson',
-                           'Fantasy',
-                           '2010-08-31',
-                           1,
-                           91,
-                           "01-01-2019",
-                           "Cosmere Stormlight Archive")
-        }
-    ]
-    return render_template('index.html', title='Home', posts=posts)
+
+    return render_template('index.html', title='Home')
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -74,4 +51,35 @@ def register():
         flash('Great! You are now a registered user! Thanks for signing up!')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'record': Book('The Lies of Locke Lamora',
+                           'Scott Lynch',
+                           'Fantasy',
+                           '2006-06-27',
+                           1,
+                           95,
+                           "01-01-2015",
+                           "Gentleman Bastards")
+        },
+        {
+            'author': {'username': 'Susan'},
+            'record': Book('The Way of Kings',
+                           'Brandon Sanderson',
+                           'Fantasy',
+                           '2010-08-31',
+                           1,
+                           91,
+                           "01-01-2019",
+                           "Cosmere Stormlight Archive")
+        }
+    ]
+    return render_template('user.html', user=user, posts=posts)
 
