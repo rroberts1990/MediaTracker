fedef41adbd93f2834f7a1e02a11f1e63a043a57
Ross Roberts <ross123roberts@gmail.com>
2022-02-20 22:41:43 -0500

    unit tests

--- tests/tests.py
+++ tests/tests.py
@@ -8,9 +8,43 @@ class TestConfig(Config):
     Testing = True
     SQLALCHEMY_DATABASE_URI = 'sqlite://'
 
-class UserModelCase:
+class TestUserModel:
+    def __init__(self):
+        self.app = create_app(TestConfig)
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        db.create_all()
 
     def test_password_hashing(self):
         u = User(username='Susan')
         u.set_password('cat')
         assert u.check_password('cat')
+
+    def test_follow(self):
+        u1 = User(username='Winston', email='winston@gmail.com')
+        u2 = User(username='Joanna', email='joanna@gmail.com')
+        db.session.add(u1)
+        db.session.add(u2)
+        db.session.commit()
+
+        assert u1.followed.all() == []
+        assert u1.followers.all() == []
+
+        u1.follow(u2)
+        db.session.commit()
+
+        assert u1.is_following(u2)
+        assert u1.followed.count() == 1
+        assert u1.followed.first().username == 'Joanna'
+        assert u2.followers.count() == 1
+        assert u2.followers.first().username == 'Winston'
+
+        u1.unfollow(u2)
+        db.session.commit()
+
+        assert not u1.is_following(u2)
+
+        assert u1.followed.count() == 0
+        assert u2.followers.count() == 0
+
+
