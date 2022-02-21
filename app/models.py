119ae9dbc7d0e2437576b69137a2ffc9c2fc4873
Ross Roberts <ross123roberts@gmail.com>
2022-02-21 14:40:54 -0500

    got rid of broken search functionality

--- app/models.py
+++ app/models.py
@@ -3,7 +3,6 @@ from flask import current_app
 from flask_login import UserMixin
 from hashlib import md5
 from werkzeug.security import check_password_hash, generate_password_hash
-from app.search import add_to_index, remove_from_index, query_index
 from datetime import datetime
 from time import time
 import jwt
@@ -13,46 +12,7 @@ followers = db.Table('followers',
                      db.Column('followed_id', db.Integer, db.ForeignKey('user.id')))
 
 
-class SearchableMixin:
-    @classmethod
-    def search(cls, expression, page, per_page):
-        ids, total = query_index(cls.__tablename__, expression, page, per_page)
-        if total == 0:
-            return cls.query.filter_by(id=0), 0
-        when = [(ids[i], i) for i in range(len(ids))]
-        return cls.query.fitler(cls.id.in_(ids)).order_by(db.case(when, value=cls.id)), total
-
-    @classmethod
-    def before_commit(cls, session):
-        session._changes = {
-            'add': list(session.new),
-            'update': list(session.dirty),
-            'delete': list(session.deleted)
-        }
-
-    @classmethod
-    def after_commit(cls, session):
-        for obj in session._changes['add']:
-            if isinstance(obj, SearchableMixin):
-                add_to_index(obj.__tablename__, obj)
-        for obj in session._changes['update']:
-            if isinstance(obj, SearchableMixin):
-                add_to_index(obj.__tablename__, obj)
-        for obj in session._changes['delete']:
-            if isinstance(obj, SearchableMixin):
-                remove_from_index(obj.__tablename__, obj)
-        session._changes = None
-
-    @classmethod
-    def reindex(cls):
-        for obj in cls.query:
-            add_to_index(cls.__tablename__, obj)
-
-
-db.event.listen(db.session, 'before_commit', SearchableMixin.before_commit)
-db.event.listen(db.session, 'after_commit', SearchableMixin.after_commit)
-
-class User(SearchableMixin, UserMixin, db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -110,7 +70,7 @@ class User(SearchableMixin, UserMixin, db.Model):
 def load_user(id):
     return User.query.get(int(id))
 
-class Book(SearchableMixin, db.Model):
+class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     author = db.Column(db.String(120))
@@ -125,7 +85,7 @@ class Book(SearchableMixin, db.Model):
     def __repr__(self):
         return f'<Book {self.title}>'
 
-class Screen(SearchableMixin, db.Model):
+class Screen(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     year = db.Column(db.Integer)
